de70a3dfe4cf5389655570e9e31eee850d464c84
tofetpuzo <tofet.femi@gmail.com>
2026-09-13 22:39:47 +0100

    feat(core): implement trading type utilities

--- src/core/Types.cpp
+++ src/core/Types.cpp
@@ -1,1 +1,17 @@
 // TODO: Story 2 (#19)
+#pragma once
+
+#include <chrono>
+#include <cstdint>
+#include <string>
+
+namespace trading 
+{
+
+using Price = double;
+using Quantity = double;
+using OrderId = uint64_t;
+using timestamp = std::chrono::steady_clock::time_point;
+
+
+}  // namespace trading
